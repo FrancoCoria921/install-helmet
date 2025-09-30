36b08c820f3b359c0f7a7a086c8766f19785b81d
FrancoCoria921 <francomartincoria921@gmail.com>
2025-09-29 22:16:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 //END_SYNC
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
 });
